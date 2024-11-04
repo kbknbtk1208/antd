a4da7b563bf20591c4dd35b26461f2dd8cbc6f2b
kuboki.nobutaka <nkuboki1208@gmai.com>
2024-11-04 14:36:32 +0900

    animation

--- front/src/app/search/searchBox.tsx
+++ front/src/app/search/searchBox.tsx
@@ -1,7 +1,8 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Address } from "./regionSearch";
 import { Button, Flex, Select, Space, Typography } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
+import { motion } from "framer-motion";
 
 type Props = {
   selectedSearchParams: {
@@ -25,7 +26,16 @@ export const SearchBox: FC<Props> = ({
   ];
 
   const InScrollingDisplay = (
-    <Flex gap={10}>
+    <Flex
+      gap={10}
+      style={{
+        position: "fixed",
+        top: "0px",
+        width: "100%",
+        zIndex: 10,
+        backgroundColor: "#ffffff",
+      }}
+    >
       <Space>
         <SearchOutlined />
         <Typography.Text>{selectedSearchParams.region.name}</Typography.Text>
@@ -37,32 +47,104 @@ export const SearchBox: FC<Props> = ({
     </Flex>
   );
 
+  const [isScrolling, setIsScrolling] = useState(false);
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolling(window.scrollY > 0);
+      setScrollY(window.scrollY);
+    };
+
+    const handleScrollEnd = () => {
+      console.log("end");
+      //   setIsScrolling(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scrollend", handleScrollEnd);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("scrollend", handleScrollEnd);
+    };
+  }, []);
+
   return (
-    <Flex
-      vertical
-      gap={16}
+    <motion.div
+      animate={{
+        height: isScrolling ? "45px" : "fit-content",
+      }}
+      transition={{ duration: 0.3 }}
       style={{
-        position: "sticky",
+        position: "fixed",
         top: "0px",
+        width: "100%",
         zIndex: 10,
         backgroundColor: "#ffffff",
+        gap: isScrolling ? "10px" : "16px",
+        justifyContent: "center",
+        padding: "5px 10px",
       }}
     >
-      <Select
-        open={false}
-        onClick={onClickRegionalSearchSelect}
-        options={regionOption}
-        value={selectedSearchParams.region.id}
-      />
-      <Select
-        open={false}
-        onClick={onClickItemNameSearchSelect}
-        value={selectedSearchParams.itemName}
-      />
-      <Select />
-      <Button style={{ backgroundColor: "#000000", color: "#ffffff" }}>
-        検索
-      </Button>
-    </Flex>
+      <motion.div
+        animate={{ width: isScrolling ? "30%" : "100%" }}
+        transition={{ duration: 0.3 }}
+      >
+        <Select
+          open={false}
+          onClick={onClickRegionalSearchSelect}
+          options={regionOption}
+          value={selectedSearchParams.region.id}
+          style={{ width: "100%" }}
+        />
+      </motion.div>
+      <motion.div
+        animate={{
+          width: isScrolling ? "30%" : "100%",
+          x: isScrolling ? "110%" : "0",
+          y: isScrolling ? "-100%" : "0",
+          paddingTop: isScrolling ? "0" : "16px",
+        }}
+        transition={{ duration: 0.3 }}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Select
+          open={false}
+          onClick={onClickItemNameSearchSelect}
+          value={selectedSearchParams.itemName}
+          style={{ width: "100%" }}
+        />
+      </motion.div>
+      <motion.div
+        animate={{
+          width: isScrolling ? "30%" : "100%",
+          x: isScrolling ? "220%" : "0",
+          y: isScrolling ? "-200%" : "0",
+          paddingTop: isScrolling ? "0" : "16px",
+        }}
+        transition={{ duration: 0.3 }}
+      >
+        <Select style={{ width: "100%" }} />
+      </motion.div>
+      <motion.div
+        animate={{ opacity: isScrolling ? 0 : 1 }}
+        transition={{ duration: 0.3 }}
+      >
+        {!isScrolling && (
+          <Button
+            style={{
+              backgroundColor: "#000000",
+              color: "#ffffff",
+              width: "100%",
+              marginTop: "16px",
+            }}
+          >
+            検索
+          </Button>
+        )}
+      </motion.div>
+    </motion.div>
   );
 };
